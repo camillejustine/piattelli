ebfb5deb232b8ab4c3593e9bd58026aac317878b
camillejustine <cj.jadermark@gmail.com>
2021-03-08 14:54:37 +0100

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import { Component } from 'react';
-import './App.css';
-import Header from './components/Header';
-import ProductList from './components/ProductList';
-import CartProvider from './contexts/CartContext';
 
 class App extends Component {
-  
   render() {
     console.log('APP RENDER');
-    return (
-        <CartProvider>
-          <div className="App">
-            <Header />
-            <ProductList />
-          </div>
-        </CartProvider>
-    );
+    return <h1>hello</h1>;
   }
 }
 
