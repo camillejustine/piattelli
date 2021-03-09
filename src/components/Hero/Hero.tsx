24a770c626d28c8fe13209ad7ac6f2a3a4ce7006
supertramps <alexander.liljedahl@live.se>
2021-03-09 13:18:51 +0100

    Added styling and text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,22 +1,48 @@
 import { CSSProperties } from "react";
-import heroPic1 from "./components/assets/hero1.png";
+import heroPic1 from "../../assets/hero1.png";
+import heroPic2 from "../../assets/hero2.png";
 
 function Footer() {
   return (
-    <div style={flexCenter}>
-      <div style={heroPic}></div>
-      <div></div>
+    <div style={heroContainer}>
+      <div style={heroPicLeft}>
+        <h2 style={heroTitle}>NEW COLLECTION</h2>
+      </div>
+      <div style={heroPicRight}>
+        <h2 style={heroTitle}>THE SOFTY</h2>
+      </div>
     </div>
   );
 }
 
-const heroPic: CSSProperties = {
+const heroPicLeft: CSSProperties = {
   backgroundImage: `url(${heroPic1})`,
+  width: "40rem",
+  height: "50rem",
+  backgroundPosition: "center",
+  objectFit: "cover",
+  marginRight: ".5rem",
 };
 
-const flexCenter: CSSProperties = {
+const heroPicRight: CSSProperties = {
+  backgroundImage: `url(${heroPic2})`,
+  width: "40rem",
+  height: "50rem",
+  backgroundPosition: "center",
+  objectFit: "cover",
+  marginLeft: ".5rem",
+};
+
+const heroContainer: CSSProperties = {
   display: "flex",
   justifyContent: "center",
+};
+
+const heroTitle: CSSProperties = {
+  color: "white",
+  position: "absolute",
+  top: "40rem",
+  marginLeft: "3rem",
 };
 
 export default Footer;
