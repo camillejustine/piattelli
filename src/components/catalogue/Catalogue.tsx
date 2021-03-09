38c3233cd6e173fd2709ca7562ed69342ff943cb
Intradastingly <asaf_lackgren@hotmail.com>
2021-03-09 14:54:45 +0100

    Catalogue framework

---
+++ src/components/catalogue/Catalogue.tsx
@@ -0,0 +1,8 @@
+function Catalogue() { 
+    return(
+        <>
+        </>
+    )
+}
+
+export default Catalogue;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,15 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Hero from "./hero/Hero";
+import Catalogue from "./catalogue/Catalogue"
+
 
 function Layout() {
   return (
     <div>
       <Header />
       <Hero />
+      <Catalogue />
       <Footer />
     </div>
   );
