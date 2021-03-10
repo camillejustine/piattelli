76c8e49493d67ba966adf20a37ce1070c150e577
supertramps <alexander.liljedahl@live.se>
2021-03-10 14:52:51 +0100

    Fixed mergebug

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,7 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Hero from "./hero/Hero";
 import Promotion from "./content/Promotion";
+import Explore from "./content/Explore";
 
 function Layout() {
   return (
@@ -9,6 +10,7 @@ function Layout() {
       <Header />
       <Hero />
       <Promotion />
+      <Explore />
       <Footer />
     </div>
   );
