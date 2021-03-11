3aa0cd8b998fc1a8cef6db408e568a88971e53d7
Sebastian Zazzi <sebastian@zazzi.se>
2021-03-11 10:56:08 +0100

    fixes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import Header from "./header/Header";
-import Footer from "./footer/Footer";
 import Hero from "./hero/Hero";
+import Footer from "./footer/Footer";
 import Promotion from "./content/Promotion";
 import Explore from "./content/Explore";
 import PromotionSecond from "./content/PromotionSecond";
