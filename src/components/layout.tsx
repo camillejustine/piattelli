7168477da5baea0fdea07f3bc6097b6ef93314b2
Intradastingly <asaf_lackgren@hotmail.com>
2021-03-11 15:05:33 +0100

    hmmmmmm

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,24 +3,36 @@ import Footer from "./footer/Footer";
 import Hero from "./hero/Hero";
 import Promotion from "./content/Promotion";
 import Catalogue from "./catalogue/Catalogue";
-import { Route} from 'react-router-dom';
-import ProductView from './product/ProductView'
+import { Route } from 'react-router-dom';
+import ProductView from './product/ProductView';
+import {useState} from 'react';
 
 function Layout() {
-  return (
-    <div>
-          <Route exact path="/">
-            <Header />
-            <Hero />
-            <Promotion />
-            <Catalogue />
-            <Footer />
-          </Route>
-          <Route path="/:name">
-              <ProductView />
-          </Route>
-    </div>
-  );
+
+  const[productDetail,setProductDetail] = useState({name: "", price: "", preview: ""})
+
+  function productDetails(value: object){
+      setProductDetail(value)
+  }
+
+    return (
+      <div>
+            <Route exact path="/">
+              <Header />
+              <Hero />
+              <Promotion />
+              <Catalogue 
+                getProduct={productDetails}
+              />
+              <Footer />
+            </Route>
+            <Route path="/:name">
+                <ProductView />
+            </Route>
+      </div>
+    );
 }
 
 export default Layout;
+
+
