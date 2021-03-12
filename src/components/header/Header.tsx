17ce2b5dc3fa2965e8193d4b35ab3d16f19c8585
Sebastian Zazzi <sebastian@zazzi.se>
2021-03-12 10:35:30 +0100

    small style changes and fixed animations

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,80 +1,86 @@
-
 import { Box, Link, TextField, Typography } from "@material-ui/core";
 import { Component, CSSProperties, useEffect, useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import { ShoppingCart as CartIcon } from "@material-ui/icons";
 import "animate.css/animate.css";
 import Cart from "./Cart";
-
+import { makeStyles } from "@material-ui/styles";
 
 function Header() {
   const [searchClicked, setSearchClicked] = useState(false);
   const [isCartVisible, setIsCartVisible] = useState(false);
+  const classes = useStyles();
 
   function hideCart() {
     setIsCartVisible(false);
   }
 
+  return (
+    <Box className={classes.rootStyle}>
+      <Link>
+        <Typography variant="body1">New Collection </Typography>
+      </Link>
+      <Link>
+        <Typography variant="body1">All Bags </Typography>{" "}
+      </Link>
 
-    return (
-        <Box style={rootStyle}>
-          <Link>New Collection</Link>
-          <Link>All Bags</Link>
+      <Link href="#">
+        <Typography variant="h1">PIATELLI</Typography>
+      </Link>
 
-          <Link>
-            <Typography variant='h1'>
-              PIATELLI
-            </Typography>
-          </Link>
-
-          <Link>Timless Favorites</Link>
-          <Box display="flex" m="1rem">
-            <Box onClick={() => setSearchClicked(true)} m="1rem">
-              {!searchClicked 
-              ? (
-                <SearchIcon />
-              ) : (
-
-                <form className="animate__animated animate__fadeIn">
-                  <TextField
-                    autoFocus
-                    onBlur={() => setSearchClicked(false)}
-                    id="standard-basic"
-                    label="Standard"
-                  />
-
-                </form>
-              )}
-            </Box>
-            <Box m="1rem">
-              {/* Icon */}
-              <CartIcon onClick={ () => {
-                setIsCartVisible(true);
-                console.log('clicked');
-                console.log(isCartVisible);
-                
-              }}/>
-              {/* Cart view */}
-              <Cart onHide={hideCart} isVisible={isCartVisible}/>
-            </Box>
-          </Box>
+      <Link>
+        <Typography variant="body1">Timless Favorites</Typography>{" "}
+      </Link>
+      <Box className={classes.iconWrapper}>
+        <Box onClick={() => setSearchClicked(true)} m="1rem">
+          {!searchClicked ? (
+            <SearchIcon />
+          ) : (
+            <form className="animate__animated animate__fadeIn">
+              <TextField
+                autoFocus
+                onBlur={() => setSearchClicked(false)}
+                id="search-basic"
+                label="Search"
+              />
+            </form>
+          )}
         </Box>
-    );
-  }
+        <Box m="1rem">
+          {/* Icon */}
+          <CartIcon
+            onClick={() => {
+              setIsCartVisible(true);
+              console.log("clicked");
+              console.log(isCartVisible);
+            }}
+          />
+          {/* Cart view */}
+          <Cart onHide={hideCart} isVisible={isCartVisible} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
 
-
-const rootStyle: CSSProperties = {
-  top: 0,
-  margin: '0',
-  padding: '0',
-  display: 'flex',
-  justifyContent: 'space-evenly',
-  alignItems: 'center',
-  position: 'fixed',
-  zIndex: 5,
-  width: '100vw',
-  height: '8.5rem',
-  backgroundColor: 'white',
-};
+const useStyles = makeStyles({
+  rootStyle: {
+    top: 0,
+    margin: "0",
+    padding: "0",
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    position: "fixed",
+    zIndex: 5,
+    width: "100vw",
+    height: "8.5rem",
+    backgroundColor: "white",
+  },
+  iconWrapper: {
+    display: "flex",
+    alignItems: "center",
+  },
+});
 
 export default Header;
