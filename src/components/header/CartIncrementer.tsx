3e56a984e5f3c041efedfac653469811d7a6754a
Intradastingly <asaf_lackgren@hotmail.com>
2021-03-19 10:46:03 +0100

    hmmmm

---
+++ src/components/header/CartIncrementer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+
+class GroupedButtons extends React.Component<{}, { counter: number }>{
+  state = { counter:  0 };
+
+  handleIncrement = () => {
+    this.setState(state => ({ counter: state.counter + 1 }));
+  };
+
+  handleDecrement = () => {
+    this.setState(state => ({ counter: state.counter - 1 }));
+  };
+
+  render() {
+    const displayCounter = this.state.counter > 0;
+
+    return (
+      <ButtonGroup size="small" aria-label="small outlined button group">
+        <Button onClick={this.handleIncrement}>+</Button>
+        {displayCounter && <Button disabled>{this.state.counter}</Button>}
+        {displayCounter && <Button onClick={this.handleDecrement}>-</Button>}
+      </ButtonGroup>
+    );
+  }
+}
+
+export default GroupedButtons;
