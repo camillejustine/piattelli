c0852bb0e377fd7371694603b627c53acb010d76
Sebastian Zazzi <sebastian@zazzi.se>
2021-03-23 15:33:29 +0100

    styled checkout and oc

--- src/components/checkout/OrderComfirmation.tsx
+++ src/components/checkout/OrderComfirmation.tsx
@@ -4,6 +4,11 @@ import React from "react";
 interface IProps {
   email: string | undefined;
   name: string | undefined;
+  adress: string | undefined;
+  phoneNumber: string | undefined;
+  zipCode: string | undefined;
+  country: string | undefined;
+  city: string | undefined;
   payedProducts: any;
   deliveryOption: string | undefined;
   total: number | undefined;
@@ -27,23 +32,32 @@ function OrderComfirmation(props: IProps) {
 
   return (
     <Box>
-      <Typography variant='h6' className={classes.centerFlex}>
-        Thank you for your purchase. An email with more information has been
-        send to {props.email}.
+      <Typography variant="h6" className={classes.centerFlex}>
+        Thank you for your purchase.
       </Typography>
-      <Typography variant='body1' className={classes.centerFlex}>Invoice ID: {invoiceID} </Typography>
+      <Typography variant="body2" className={classes.centerFlex}>
+        An email with more information has been send to {props.email}.
+      </Typography>
+      <Typography variant="body1" className={classes.centerFlex}>
+        Invoice ID: {invoiceID}{" "}
+      </Typography>
       <Box className={classes.centerFlex}>
-        <Box className={classes.cartContentWrapper}>
-          {props.payedProducts.map((product: any) => (
-            <Box className={classes.cartContent}>
-              <img src={product.preview} width="100rem" height="100rem" />
-              <div className={classes.productInfo}>
-                <Typography variant="body1">{product.name}</Typography>
-                <Typography variant="body2">Price: {product.price}</Typography>
-              </div>
-            </Box>
-          ))}
-          <Typography>
+        <Box>
+          <Box className={`${classes.cartContentWrapper} ${classes.margin2}`}>
+            {props.payedProducts.map((product: any) => (
+              <Box className={`${classes.cartContent}`}>
+                <img src={product.preview} width="100rem" height="100rem" />
+                <div className={classes.productInfo}>
+                  <Typography variant="body1">{product.name}</Typography>
+                  <Typography variant="body1">x{product.quantity}</Typography>
+                  <Typography variant="body2">
+                    Price: {product.price}
+                  </Typography>
+                </div>
+              </Box>
+            ))}
+          </Box>
+          <Typography className={classes.margin2}>
             Total price: {props.total}kr + shipping cost: (
             {props.deliveryOption === "pn" ? "0kr" : null}
             {props.deliveryOption === "budbee" ? "69kr" : null}
@@ -51,27 +65,33 @@ function OrderComfirmation(props: IProps) {
           </Typography>
         </Box>
         <Box>
-          <Box>
-            <Typography>Customer:</Typography>
-            <Typography>Name: props.name</Typography>
-            
+          <Box className={classes.margin2}>
+            <Typography variant="h6">Customer:</Typography>
+            <Typography>Name: {props.name}</Typography>
+            <Typography>
+              Adress: {props.adress}, {props.zipCode} {props.city},{" "}
+              {props.country}
+            </Typography>
+            <Typography>Telefon: {props.phoneNumber}</Typography>
           </Box>
-          {props.deliveryOption === "pn" ? (
-            <Typography>
-              Delivery method: <br /> Delivery to closest Postombud with Post
-              Nord.
-            </Typography>
-          ) : null}
-          {props.deliveryOption === "budbee" ? (
-            <Typography>
-              Delivery method: <br /> Home delivery with Budbee.
-            </Typography>
-          ) : null}
-          {props.deliveryOption === "instabox" ? (
-            <Typography>
-              Delivery method: <br /> Delivery to closest Instabox.
-            </Typography>
-          ) : null}
+          <Box className={classes.margin2}>
+            {props.deliveryOption === "pn" ? (
+              <Typography>
+                Delivery method: <br /> Delivery to closest Postombud with Post
+                Nord.
+              </Typography>
+            ) : null}
+            {props.deliveryOption === "budbee" ? (
+              <Typography>
+                Delivery method: <br /> Home delivery with Budbee.
+              </Typography>
+            ) : null}
+            {props.deliveryOption === "instabox" ? (
+              <Typography>
+                Delivery method: <br /> Delivery to closest Instabox.
+              </Typography>
+            ) : null}
+          </Box>
         </Box>
       </Box>
     </Box>
@@ -86,13 +106,31 @@ const useStyles = makeStyles({
   },
   cartContentWrapper: {
     overflow: "auto",
+    height: "25rem",
+    '&::-webkit-scrollbar': {
+      width: '0.4em'
+    },
+    '&::-webkit-scrollbar-track': {
+      boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+      webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.1)',
+      outline: '1px solid slategrey',
+      border:' 4px solid transparent',
+      borderRadius:'8px',
+      backgroundClip: 'padding-box',
+    }
   },
   cartContent: {
-    margin: "1rem 2rem",
+    margin: "1rem 0rem",
     display: "flex",
   },
   productInfo: {
     marginLeft: "1rem",
+  },
+  margin2: {
+    margin: "2rem",
   },
 });
 
